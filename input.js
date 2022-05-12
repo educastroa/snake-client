441dec95313343b68120c48b2c46111d19765e20
Eduardo <eduardocastro.a@hotmail.com>
2022-05-12 02:11:12 +0000

    exported setupInput to play

---
+++ input.js
@@ -0,0 +1,25 @@
+const setupInput = function () {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    console.log('hi');
+  };
+  
+  
+  return stdin;
+};
+
+
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,36 +1,5 @@
 const { connect } = require('./client');
-
-const setupInput = function () {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-
-
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-    console.log('hi');
-  };
-  
-  
-  return stdin;
-};
-
-
-
-
-
-
-
-
-
-
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
