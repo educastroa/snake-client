9e65a3a8e65603f35068aec3a4b44238df25d665
Eduardo <eduardocastro.a@hotmail.com>
2022-05-12 02:04:14 +0000

    add setup input funtion

--- play.js
+++ play.js
@@ -1,6 +1,37 @@
-
 const { connect } = require('./client');
+
+const setupInput = function () {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    console.log('hi');
+  };
+  
+  
+  return stdin;
+};
+
+
+
+
+
+
+
+
+
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
